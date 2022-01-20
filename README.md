7a6e48b439984a30f2f5999c8fa1f60606f65c26
digplan <digplan@outlook.com>
2022-01-20 09:52:36 -0600

    3 files modified

--- README.md
+++ README.md
@@ -3,8 +3,16 @@
 Zero dependency, pure JS, tiny JWT for servers.
 
 ```js
+const {JWT} = import('jwt-tiny');
+const secret = 'your-256-bit-secret';
+const payload = {
+  "sub": "1234567890",
+  "name": "John Doe",
+  "iat": 1516239022
+};
+
 const jwt = new JWT(secret);
 const jwtstr = jwt.create(payload);
-console.log(jwtstr);  // JWT string
-console.log(jwt.verify(jwtstr));  // Payload, or false if invalid signature
+const payload = jwt.verify(jwtstr);
+console.log(jwtstr, payload);
 ```
--- test.mjs
+++ test.mjs
@@ -2,17 +2,14 @@ import { JWT } from './index.mjs';
 
 // Don't use this secret for anything. Testing only.
 
-const secret =
-  'NTNv7j0TuYARvmNMmWXo6fKvM4o6nv/aUi9ryX38ZH+L1bkrnD1ObOQ8JAUmHCBq7Iy7otZcyAagBLHVKvvYaIpmMuxmARQ97jUVG16Jkpkp1wXOPsrF9zwew6TpczyHkHgX5EuLg2MeBuiT/qJACs1J0apruOOJCg/gOtkjB4c=';
+const secret = 'your-256-bit-secret';
 const payload = {
   sub: '1234567890',
   name: 'John Doe',
-  admin: true,
-  iat: 1642638486,
-  exp: 1642642086,
+  iat: 1516239022,
 };
 
 const jwt = new JWT(secret);
 const jwtstr = jwt.create(payload);
-console.log(jwtstr);
-console.log(jwt.verify(jwtstr));
+const verify = jwt.verify(jwtstr);
+console.log(jwtstr, verify);
